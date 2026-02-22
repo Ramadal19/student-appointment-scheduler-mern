805aa9a70951bdbfd4b10809cb95078e12adfdef
Ramadal19 <ramadal@gmail.com>
2026-02-22 01:49:58 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+
 import ProtectedRoute from "./components/ProtectedRoute";
 import DashboardV2 from "./pages/DashboardV2";
 
