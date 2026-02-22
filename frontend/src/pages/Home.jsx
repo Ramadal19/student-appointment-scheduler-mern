833e051d4c0766216683082c25c17c6c34d89e5f
Ramadal19 <ramadal@gmail.com>
2026-02-22 12:37:55 -0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import heroBg from "../assets/hero-bg.png";
 import "../styles/home.css";
 
 export default function Home() {
-  const [message, setMessage] = useState("");
   const navigate = useNavigate();
-
-  const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
-
-  useEffect(() => {
-    fetch(`${API_BASE}/api/health`, { credentials: "include" })
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error("API ERROR:", err));
-  }, [API_BASE]);
 
   return (
     <div
@@ -23,16 +13,15 @@ export default function Home() {
     >
       <div className="overlay">
         <div className="content">
-          <h1>Student Appointment <br />
-           Scheduler</h1>
+          <h1>
+            Student Appointment <br />
+            Scheduler
+          </h1>
 
           <p>
-            Book academic advising appointments easily <br />
-            and manage your
+            Book academic advising appointments easily and manage your
             schedule in one place.
           </p>
-
-          {message && <p className="health">{message}</p>}
 
           <div className="buttons">
             <button
